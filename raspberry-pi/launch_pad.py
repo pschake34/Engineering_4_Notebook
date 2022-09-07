41eb157ad7050754b5fe3bef28a5a4f163e28e7b
pschake34 <pschake34@charlottesvilleschools.org>
2022-09-07 10:28:24 -0400

    Add lights

--- raspberry-pi/launch_pad.py
+++ raspberry-pi/launch_pad.py
@@ -3,13 +3,24 @@
 
 import board
 import time
+import digitalio
+
+led_red = digitalio.DigitalInOut(board.GP13)
+led_green = digitalio.DigitalInOut(board.GP18)
+led_red.direction = digitalio.Direction.OUTPUT
+led_green.direction = digitalio.Direction.OUTPUT
 
 def countdown(x):
+    print("Starting Countdown...")
     while x > 0:
         print(f"{x} seconds left...")
         x -= 1
-        time.sleep(1)
+        led_red.value = True
+        time.sleep(.5)
+        led_red.value = False
+        time.sleep(.5)
     print("Liftoff!")
+    led_green.value = True
+    time.sleep(10)
 
-print("Starting Countdown...")
 countdown(10)
