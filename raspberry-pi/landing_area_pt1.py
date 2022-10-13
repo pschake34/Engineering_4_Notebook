1c0de351210da5bbbce7f4fcd26e3350bb822e9b
pschake34 <pschake34@charlottesvilleschools.org>
2022-10-13 09:47:30 -0400

    Add comments to landing area part 1

--- raspberry-pi/landing_area_pt1.py
+++ raspberry-pi/landing_area_pt1.py
@@ -1,13 +1,13 @@
 # Paul Schakel
 # Landing Area Part 1
 
-def get_input():
+def get_input(): # Takes user input of 3 sets of coordinates
     print("Enter 3 sets of coordinates: ")
     coordinates = []
-    try:
+    try: # Makes sure that the user is entering the correct syntax
         for i in range(3):
             inp = input(f"Enter coordinate set {i} in x,y format: ")
-            coordinate = inp.split(',')
+            coordinate = inp.split(',') # Splits the input string into its 2 numbers
             print(coordinate)
             x = float(coordinate[0])
             y = float(coordinate[1])
@@ -17,7 +17,7 @@ def get_input():
         print("These points are not a valid triangle. Please try again, and make sure you are using the x,y syntax!\n\n")
         return get_input()
 
-def calculate(coordinates):
+def calculate(coordinates): # Calculates the area or a triangle from the coordinates of its vertices
     print(coordinates)
     area = 1/2*abs(coordinates[0][0] * (coordinates[1][1] - coordinates[2][1]) + coordinates[1][0] * (coordinates[2][1] - coordinates[0][1]) + coordinates[2][0] * (coordinates[0][1] - coordinates[1][1]))
     return area
