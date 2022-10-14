545e6f01a057405e5a07aeb23e91ceb14090d2f8
pschake34 <pschake34@charlottesvilleschools.org>
2022-10-14 10:14:07 -0400

    Finish morse code part 1

---
+++ raspberry-pi/morse_code_pt1.py
@@ -0,0 +1,31 @@
+# Paul Schakel
+# Morse Code Part 1
+
+# Dictionary representing the morse code chart
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+
+while True:
+    to_translate = input("Enter the phrase you would like translated: ")
+    if to_translate == "-q": # Quit the program
+        break
+    translated = ""
+    for char in to_translate: # Loop through message
+        if char == " ": # Add "/" between words
+            translated += "/"
+        else:
+            translated += MORSE_CODE[char.upper()] + " " # Translate character to morse code
+    print(translated + "\n\n")
